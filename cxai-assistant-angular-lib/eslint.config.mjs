af4c1d602ae11023729dfc35dc3d11b1a15b9826
Marcin Jakubowski <marcin.jakubowski@sap.com>
2025-09-24 09:31:04 +0200

    Cleanup eslint.config.mjs

--- cxai-assistant-angular-lib/eslint.config.mjs
+++ cxai-assistant-angular-lib/eslint.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 import tsparser from "@typescript-eslint/parser";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+    baseDirectory: import.meta.dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
@@ -26,7 +22,6 @@ export default defineConfig([{
     extends: compat.extends(
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        // "plugin:@typescript-eslint/recommended-type-checked",
         "plugin:@angular-eslint/recommended",
         "plugin:@angular-eslint/template/process-inline-templates",
     ),
